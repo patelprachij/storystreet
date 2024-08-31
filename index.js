665f8498c3ad3af559d04564ee001e2efd8d893b
patelprachij <137316246+patelprachij@users.noreply.github.com>
2024-08-31 09:19:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//🔸 Modal window function
+
+// ^ querySelector 
+const nav_open_window_button = document.querySelector(".nav_open_window_button");
+const nav_closed_window_button = document.querySelectorAll(".nav_closed_window_button");
+
+// ^ addEventListener 
+nav_open_window_button.addEventListener("click", function() {
+    document.body.classList.add("js-when-button-been-clicked")
+});
+
+nav_closed_window_button.forEach(el => {
+    el.addEventListener('click', function() {
+        document.body.classList.remove('js-when-button-been-clicked')
+        console.log("remove");
+    });
+});
+
+console.log("Thanks for visiting");
+
+//🔸 Remove hover effect on main button 
